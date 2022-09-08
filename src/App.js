25daaf5ace5829d10576ec7e9a6b52865b1e3ad2
Oscar Uwayo <oscaruwayo@gmail.com>
2022-09-08 12:34:16 +0200

    Initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react';
+import {Link,Outlet} from 'react-router-dom'; 
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 5px",
+          paddingBottom: "1rem",
+          textDecoration: 'none'
+        }}
+      >
+        <Link style={{textDecoration: 'none'}} to="/invoices">Invoices</Link> |{"  "}
+        <Link style={{textDecoration: 'none'}} to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
-
-export default App;
